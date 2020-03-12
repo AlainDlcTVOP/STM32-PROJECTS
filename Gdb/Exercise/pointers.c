ddbca77138d79ff24d693de8dcf277cf80111c8f
AlainDlcTVOP <alain_dlc@outlook.com>
2020-03-12 18:35:53 +0100

    exercise 18:36

---
+++ Gdb/Exercise/pointers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/* 1. Create a char type variable and initialize it to value 100
+   2. Print the address of the above variable
+   3. Create a pointer variable and store the address of the above variable
+   4. Perform read operation on the pointer variable to fetch 1 byte of data from the pointer
+   5. Print the data obtained from the read operation on the pointer
+   6. Perform write operation on the pointer to stgore the value 65
+   7. Print the value of the variable defined in step 1 */
+
+int main()
+{
+    char data = 100;
+    printf("Value = %d\n", data);
+    printf("Adress of value = %p\n", data);
+
+    //Create a pointer variable and store the address of the above variable
+    char *pAddress = &data;
+    char value = *pAddress;
+    *pAddress = 65;
+
+    printf("Adress of value = %p\n", value);
+    printf("Value = %d\n", data);
+
+    return 0;
+}
